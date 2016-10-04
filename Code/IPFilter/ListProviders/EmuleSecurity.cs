98ec6d3f790340e8ee841a4f7554551d0950c56b
Alexander Georgievskiy <galeksandrp@gmail.com>
2016-10-04 13:08:39 +0000

    Update EmuleSecurity.cs

--- Code/IPFilter/ListProviders/EmuleSecurity.cs
+++ Code/IPFilter/ListProviders/EmuleSecurity.cs
@@ -2,11 +2,14 @@ namespace IPFilter.ListProviders
 {
     using System;
     using System.Collections.Generic;
+    using System.Globalization;
     using Models;
 
     public class EmuleSecurity : IMirrorProvider
     {
-        static readonly IEnumerable<FileMirror> mirrors = new[] { new FileMirror("Default", "Emule Security") };
+        static readonly IEnumerable<FileMirror> mirrors = new[] { new FileMirror("ipfilter.zip", "Emule Security"),
+        new FileMirror("fakeservers.txt", "Fake servers list (part one)"),
+        new FileMirror("Inclusions.txt", "Fake servers list (part two)") };
 
         /// <summary>
         /// Gets a list of mirrors for this provider
@@ -24,12 +27,7 @@ namespace IPFilter.ListProviders
 
         public string GetUrlForMirror(FileMirror mirror)
         {
-            if(string.Equals("Default", mirror.Id, StringComparison.OrdinalIgnoreCase))
-            {
-                return "http://upd.emule-security.org/ipfilter.zip";
-            }
-
-            throw new ArgumentOutOfRangeException();
+            return string.Format(CultureInfo.CurrentCulture, "http://upd.emule-security.org/{0}", mirror.Id);
         }
     }
 }
